51ce47dacee5c3d677838d54b89ac96d653d15b7
Miguel Pieters <miguelpieters@MBP-van-Miguel.home>
2021-03-03 22:43:54 +0100

    datta controllers

---
+++ src/api/controllers/datamart.ts
@@ -0,0 +1,30 @@
+import programmes from "../store/dataset.json";
+import locations from "../store/locations.json";
+import School from "../../models/School";
+
+export default class DatamartController {
+
+  public static getData = (school?: string): School[] => {
+    let data = programmes.sort((a, b) =>
+      (a.programme || "").localeCompare(b.programme || "")).filter(p => p.programme !== null)
+
+    if (school) {
+      data = data.filter(x => x.school.toLowerCase().includes(school.toLowerCase()));
+    }
+
+    return data;
+  }
+
+  public static getLocations = () => {
+    return locations;
+  }
+
+  public static getLocation = (name: string) => {
+    if (!Object.keys(locations).includes(name)) return null;
+    return (locations as any)[name];
+  }
+
+  public static getSchools = () => {
+    return Object.keys(locations);
+  }
+}
