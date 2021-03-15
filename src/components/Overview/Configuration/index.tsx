e706d72a927fba204b56a2a501718437b5714985
Miguel Pieters <miguelpieters@MBP-van-Miguel.home>
2021-03-15 21:03:19 +0100

    Feedback Gerylaine

--- src/components/Overview/Configuration/index.tsx
+++ src/components/Overview/Configuration/index.tsx
@@ -9,7 +9,8 @@ type IState = {
   continent: string | null;
   country: string | null;
   field: string | null,
-  programme: string | null
+  programme: string | null,
+  track: string | null
 }
 
 export default class Configuration extends Component<{
@@ -21,15 +22,17 @@ export default class Configuration extends Component<{
     country: null,
     field: null,
     programme: null,
+    track: null
   }
 
   update = () => {
     let schools: School[] = DatamartController.getData();
-    const { country, field, programme } = this.state;
+    const { country, field, programme, track } = this.state;
 
     if (field) schools = schools.filter(s => s.field === field);
     if (programme) schools = schools.filter(s => s.programme === programme);
     if (country) schools = schools.filter(s => s.country === country);
+    if (track) schools = schools.filter(s => s.track === track);
 
     this.props.setSchools(schools);
   }
@@ -51,10 +54,16 @@ export default class Configuration extends Component<{
       (item, i, ar) => ar.indexOf(item) === i
     );
 
+    const tracks = data.filter(
+      p => this.state.track ? p.track === this.state.track : true
+    ).map(p => p.track).filter(
+      (item, i, ar) => ar.indexOf(item) === i
+    );
+
     return (
       <div id={"configuration"}>
         <Row gutter={[10, 10]}>
-          <Col xs={12} md={6}>
+          <Col flex={1}>
             <Select placeholder={"Region"}
                     isClearable={true}
                     options={[
@@ -67,7 +76,7 @@ export default class Configuration extends Component<{
                     ]}
             />
           </Col>
-          <Col xs={12} md={6}>
+          <Col flex={1}>
             <Select placeholder={"Country"}
                     value={(this.state.country ?
                         { label: this.state.country!, value: this.state.country! } : null
@@ -79,7 +88,17 @@ export default class Configuration extends Component<{
                     }))}
             />
           </Col>
-          <Col xs={12} md={6}>
+          <Col flex={1}>
+            <Select placeholder={"Track"}
+                    options={tracks.map(t => ({ label: t, value: t}))}
+                    isClearable={true}
+                    value={(this.state.track ?
+                        { label: this.state.track!, value: this.state.track! } : null
+                    )}
+                    onChange={(e) => this.setState({ track: e ? e.value : null }, this.update)}
+            />
+          </Col>
+          <Col flex={1}>
             <Select placeholder={"Field of study"}
                     options={fields.map(f => ({ label: f, value: f}))}
                     value={(this.state.field ?
@@ -89,7 +108,7 @@ export default class Configuration extends Component<{
                     onChange={(e) => this.setState({ field: e ? e.value : null }, this.update)}
             />
           </Col>
-          <Col xs={12} md={6}>
+          <Col flex={1}>
             <Select placeholder={"Major"}
                     options={programmes.map(p => ({ label: p, value: p}))}
                     isClearable={true}
